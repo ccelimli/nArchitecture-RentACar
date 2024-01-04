72b7b5796bbaec79f41d2b87d90cd274497d6b9d
Çağatay Çelimli <c.celimli@live.co.uk>
2024-01-05 00:59:33 +0300

    Caching

--- Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
+++ Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Services.Repositories;
 using AutoMapper;
+using Core.Application.Pipelines.Caching;
 using Core.Application.Requests;
 using Core.Application.Responses;
 using Core.Persistence.Paging;
@@ -13,11 +14,17 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Queries.GetList;
 
-public class GetListBrandQuery:IRequest<GetListResponse<GetListBrandListItemDto>>
+public class GetListBrandQuery : IRequest<GetListResponse<GetListBrandListItemDto>>, ICacheableRequest
 {
-    public PageRequest PageRequest{ get; set; }
+    public PageRequest PageRequest { get; set; }
 
-    public class GetListBrandQueryHandler:IRequestHandler<GetListBrandQuery, GetListResponse<GetListBrandListItemDto>>
+    public string CacheKey => $"GetListBrandQuery({PageRequest.PageIndex}, {PageRequest.PageSize})";
+
+    public bool BypassCache { get; }
+
+    public TimeSpan? SlidingExpiration { get; }
+
+    public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, GetListResponse<GetListBrandListItemDto>>
     {
         private readonly IBrandRepository _brandRepository;
         private readonly IMapper _mapper;
@@ -30,11 +37,11 @@ public class GetListBrandQuery:IRequest<GetListResponse<GetListBrandListItemDto>
 
         public async Task<GetListResponse<GetListBrandListItemDto>> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
         {
-          Paginate<Brand> brands=  await _brandRepository.GetListAsync(
-                    index:request.PageRequest.PageIndex,
-                    size: request.PageRequest.PageSize,
-                    cancellationToken:cancellationToken
-                );
+            Paginate<Brand> brands = await _brandRepository.GetListAsync(
+                      index: request.PageRequest.PageIndex,
+                      size: request.PageRequest.PageSize,
+                      cancellationToken: cancellationToken
+                  );
 
             GetListResponse<GetListBrandListItemDto> response = _mapper.Map<GetListResponse<GetListBrandListItemDto>>(brands);
             return response;
